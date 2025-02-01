58cc5f08c363c974419a9171173e0fe4ac23fc81
Sarthak Panigrahi <sipunp70@gmail.com>
2025-02-01 22:18:04 +0530

    router and controller with debugging

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ dotenv.config({
 
 connectDb()
 .then(()=>{
-  app.listen(process.env.PORT || 8000,()=>{
+  app.listen(process.env.PORT || 4001,()=>{
     console.log(`Server is running at port: ${process.env.PORT}`);
     
   })
